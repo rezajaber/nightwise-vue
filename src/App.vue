535733eec1226d1829ed250da7a73faddbe58642
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-13 03:46:55 +0100

    set project fundamentals

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="bg-primary h-screen">
+    <RouterView />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#222831",
+      secondary: "#31363F",
+      accent: "#76ABAE",
+      alternative: "#EEEEEE",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
