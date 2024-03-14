22feba569a6dafd495bdbe1df6b0f19e00d66e95
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-14 20:04:22 +0100

    sort file names/paths/imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import SearchTask from "../components/SearchTask.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome class="mt-4" />
+    <SearchTask class="mt-4" />
   </main>
 </template>
