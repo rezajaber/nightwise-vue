dc55065932fe3084181a199e5cf02174a12eec34
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-14 19:50:33 +0100

    fix styling - search-tasks section

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -38,11 +38,32 @@ const position = ref("This Week");
     </div>
 
     <div class="flex gap-2.5">
-      <Button size="sm">Show done</Button>
+      <Button size="sm" class="w-1/3">Show done</Button>
 
       <DropdownMenu>
         <DropdownMenuTrigger as-child>
-          <Button size="sm" variant="outline"> {{ position }} </Button>
+          <Button
+            size="sm"
+            class="w-2/3 justify-between px-4"
+            variant="outline"
+          >
+            {{ position }}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-down"
+            >
+              <path d="M12 5v14" />
+              <path d="m19 12-7 7-7-7" />
+            </svg>
+          </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent class="w-56">
           <DropdownMenuRadioGroup v-model="position">
