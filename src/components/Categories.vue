6c14750132f14a221ab2adf962017aba04213091
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-15 22:16:58 +0100

    create categories

---
+++ src/components/Categories.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  TagsInput,
+  TagsInputItem,
+  TagsInputItemDelete,
+  TagsInputItemText,
+} from "@/components/ui/tags-input";
+
+const modelValue = ref(["Apple", "Banana"]);
+</script>
+
+<template>
+  <TagsInput v-model="modelValue" class="border-none px-5 py-0">
+    <TagsInputItem v-for="item in modelValue" :key="item" :value="item">
+      <TagsInputItemText />
+      <TagsInputItemDelete />
+    </TagsInputItem>
+  </TagsInput>
+</template>
--- src/components/OverdueTask.vue
+++ src/components/OverdueTask.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Checkbox } from "@/components/ui/checkbox";
 
+import Categories from "../components/Categories.vue";
 import OverdueTaskCounter from "../components/OverdueTaskCounter.vue";
 </script>
 
@@ -9,15 +10,18 @@ import OverdueTaskCounter from "../components/OverdueTaskCounter.vue";
     <OverdueTaskCounter />
 
     <div class="mt-5 grid gap-5 pl-4">
-      <div class="flex items-center space-x-2">
-        <Checkbox class="border-red-700 data-[state=checked]:bg-red-600" />
-        <label
-          class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          create Header
-        </label>
-      </div>
+      <div class="grid gap-1.5">
+        <div class="flex items-center space-x-2">
+          <Checkbox class="border-red-700 data-[state=checked]:bg-red-600" />
 
+          <label
+            class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            create Header
+          </label>
+        </div>
+        <Categories />
+      </div>
       <div class="flex items-center space-x-2">
         <Checkbox class="border-red-700 data-[state=checked]:bg-red-600" />
         <label
