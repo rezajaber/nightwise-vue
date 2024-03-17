178e197eeeaf799fa73703ee06cf99ec39dcecdd
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-17 23:11:18 +0100

    fix code

--- src/components/taskoverview/TaskList.vue
+++ src/components/taskoverview/TaskList.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import { Checkbox } from "@/components/ui/checkbox";
 import { Separator } from "@/components/ui/separator";
-
-const ontimeTasks = [
-  { id: 1, label: "write the bachelor thesis" },
-  { id: 2, label: "be on discord" },
-  { id: 3, label: "visit volleyball tomorrow" },
-];
-
-const overdueTasks = [
-  { id: 1, label: "write the bachelor thesis" },
-  { id: 2, label: "be on discord" },
-  { id: 3, label: "visit volleyball tomorrow" },
-];
+import Task from "./TaskCheckbox.vue";
 </script>
 
 <template>
@@ -26,43 +14,6 @@ const overdueTasks = [
       <Separator class="mt-1 bg-primary" />
     </div>
 
-    <div v-for="task in ontimeTasks" key="task.id" class="mt-5 grid gap-5 pl-4">
-      <div class="flex items-center space-x-2">
-        <Checkbox />
-        <label
-          class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          {{ task.label }}
-        </label>
-      </div>
-    </div>
-
-    <div class="mt-9">
-      <div class="flex justify-between">
-        <p class="align-middle text-sm font-semibold text-red-700">Overdue</p>
-        <p class="text-sm font-semibold text-red-700">2</p>
-      </div>
-
-      <Separator class="mt-1 bg-red-600" />
-    </div>
-
-    <div
-      v-for="task in overdueTasks"
-      :key="task.id"
-      class="mt-5 grid gap-5 pl-4"
-    >
-      <div class="grid gap-1.5">
-        <div class="flex items-center space-x-2">
-          <Checkbox class="border-red-700 data-[state=checked]:bg-red-600" />
-
-          <label
-            class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            {{ task.label }}
-          </label>
-        </div>
-        <Categories />
-      </div>
-    </div>
+    <Task />
   </div>
 </template>
