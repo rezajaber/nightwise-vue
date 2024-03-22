d58b889a25d97ab205a46706b88a5768fabd6366
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-22 04:07:08 +0100

    set task-animation

--- src/components/taskoverview/TaskList.vue
+++ src/components/taskoverview/TaskList.vue
@@ -18,7 +18,7 @@ function truncateText(text: string, limit: number = 50) {
 
 <template>
   <div>
-    <div>
+    <div class="">
       <div class="flex justify-between text-primary">
         <p class="align-middle text-sm font-semibold">Ontime</p>
         <p class="text-sm font-semibold">{{ tasks.length }}</p>
@@ -27,7 +27,11 @@ function truncateText(text: string, limit: number = 50) {
       <Separator class="mt-1 bg-primary" />
     </div>
 
-    <div v-for="task in tasks" :key="task.id" class="mt-5 grid gap-2 pl-4">
+    <div
+      v-for="task in tasks"
+      :key="task.id"
+      class="mt-5 grid cursor-pointer gap-2 rounded-lg pl-4 duration-300 ease-in-out hover:scale-105"
+    >
       <div class="flex items-center space-x-2 text-primary">
         <Checkbox />
 
