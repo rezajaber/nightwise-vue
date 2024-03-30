37cd7341bab2ff5ac588ac7edc3f57baf6d04b1b
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-30 02:04:19 +0100

    set Toaster - shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-
+import Toaster from "@/components/ui/toast/Toaster.vue";
 import Header from "../src/components/NightwiseHeader.vue";
 </script>
 
 <template>
+  <Toaster />
+
   <div
     class="dark grid min-h-screen place-content-center gap-5 bg-background px-4 py-5"
   >
