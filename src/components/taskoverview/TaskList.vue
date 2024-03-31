175744c21cbb8c76fb671ed8740aa391bde755c3
Reza Jaber <frontendrezajaber@gmail.com>
2024-03-31 05:48:37 +0200

    functionality: list created task

--- src/components/taskoverview/TaskList.vue
+++ src/components/taskoverview/TaskList.vue
@@ -2,9 +2,19 @@
 import { Separator } from "@/components/ui/separator";
 import { Checkbox } from "@/components/ui/checkbox";
 
+import { onMounted, ref } from "vue";
+import { useTaskStore } from "@/stores/taskStore";
+
 function truncateText(text: string, limit: number = 50) {
   return text.length > limit ? text.slice(0, limit) + "..." : text;
 }
+const taskStore = useTaskStore();
+const tasks = ref([]);
+
+onMounted(async () => {
+  await taskStore.fetchTasks();
+  tasks.value = taskStore.tasks;
+});
 </script>
 
 <template>
@@ -12,7 +22,7 @@ function truncateText(text: string, limit: number = 50) {
     <div>
       <div class="flex justify-between text-primary">
         <p class="align-middle text-sm font-semibold">Ontime</p>
-        <p class="text-sm font-semibold">2</p>
+        <p class="text-sm font-semibold">{{ tasks.length }}</p>
       </div>
 
       <Separator class="mt-1 bg-primary" />
@@ -20,20 +30,22 @@ function truncateText(text: string, limit: number = 50) {
 
     <div class="no-scrollbar h-96 overflow-x-hidden overflow-y-scroll">
       <div
+        v-for="task in tasks"
+        :key="task.id"
         class="mt-5 grid cursor-pointer gap-1.5 rounded-lg pl-4 duration-300 ease-in-out hover:scale-105"
       >
         <div class="flex items-center space-x-2 text-primary">
-          <Checkbox />
+          <Checkbox v-model="task.completed" />
 
           <label
             class="break-all text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
           >
-            Test
+            {{ truncateText(task.title) }}
           </label>
         </div>
 
         <p class="break-all pl-6 text-justify text-xs text-primary opacity-60">
-          Test
+          {{ truncateText(task.description) }}
         </p>
       </div>
     </div>
