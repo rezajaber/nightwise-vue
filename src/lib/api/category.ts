6523e444fa278f90f2ebac8209ff058b024f76bc
Reza Jaber <frontendrezajaber@gmail.com>
2024-04-02 22:40:57 +0200

    clean up category.ts

--- src/lib/api/category.ts
+++ src/lib/api/category.ts
@@ -2,44 +2,50 @@ import Base from "./base";
 
 export const getCategory = async (): Promise<any[]> => {
   try {
-    // you can also fetch all records at once via getFullList
     const records = await Base.getPocketbase()
       .collection("category")
-      .getFullList({
-        sort: "-created",
-      });
+      .getFullList({ sort: "-created" });
     return records;
   } catch (error) {
-    console.error("Failed to fetch tasks:", error);
+    console.error("Failed to fetch categories:", error);
     return [];
   }
 };
 
 export const createCategory = async (name: string): Promise<any> => {
-  const data = {
-    name: name,
-  };
-  const record = await Base.getPocketbase().collection("category").create(data);
-  return record;
+  try {
+    const data = { name }; // Simplify object assignment
+    const record = await Base.getPocketbase()
+      .collection("category")
+      .create(data);
+    return record;
+  } catch (error) {
+    console.error("Failed to create category:", error);
+    throw error; // Propagate the error for handling at the calling site
+  }
 };
 
 export const updateCategory = async (
-  name: string,
   category_id: string,
+  name: string,
 ): Promise<any> => {
-  const data = {
-    name: name,
-  };
-  const record = await Base.getPocketbase()
-    .collection("category")
-    .update(category_id, data);
-  return record;
+  try {
+    const data = { name }; // Simplify object assignment
+    const record = await Base.getPocketbase()
+      .collection("category")
+      .update(category_id, data);
+    return record;
+  } catch (error) {
+    console.error("Failed to update category:", error);
+    throw error; // Propagate the error for handling at the calling site
+  }
 };
 
 export const deleteCategory = async (category_id: string): Promise<void> => {
   try {
     await Base.getPocketbase().collection("category").delete(category_id);
   } catch (error) {
-    console.error("Failed to delete tasks:", error);
+    console.error("Failed to delete category:", error);
+    // Consider rethrowing or handling the error based on your application's needs
   }
 };
