6a3be2b28321d520af44fb6fee9148bd2493a293
Reza Jaber <frontendrezajaber@gmail.com>
2024-04-03 13:18:30 +0200

    add video to info button

--- src/components/taskcreation/ControlBar.vue
+++ src/components/taskcreation/ControlBar.vue
@@ -174,25 +174,27 @@ const buttonLabel = computed(() =>
     </div>
 
     <div class="flex items-center gap-2">
-      <Button
-        size="xs"
-        class="gap-1.5 border-none bg-accent tracking-wide text-white"
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#ffffff"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-info"
-        >
-          <circle cx="12" cy="12" r="10" />
-          <path d="M12 16v-4" />
-          <path d="M12 8h.01" /></svg
-      ></Button>
+      <a href="https://youtu.be/wTg3BmgRi9g" target="_blank">
+        <Button
+          size="xs"
+          class="gap-1.5 border-none bg-accent tracking-wide text-white"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ffffff"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-info"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M12 16v-4" />
+            <path d="M12 8h.01" /></svg
+        ></Button>
+      </a>
     </div>
   </div>
 </template>
